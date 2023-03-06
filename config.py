1d76fc80702bcdfc233a0f6dbd848a6a03b8577c
Natã Vitor Vaguel Reis <59771238+NVPanda@users.noreply.github.com>
2023-03-05 22:23:44 -0300

    Refazendo do zero. Fui trolado por uma estrutura.

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'CHAVE-SECRETA-GERADA-ALEATORIAMENTE'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///pdv.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
